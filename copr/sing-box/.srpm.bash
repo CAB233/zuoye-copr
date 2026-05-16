f14c707d31b3938162c3fcdcbf529c6e33709f99
CAB233 <yidaduizuoye@outlook.com>
2026-05-16 21:02:42 +0800

    build: update build scripts

--- copr/sing-box/.srpm.bash
+++ copr/sing-box/.srpm.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf -y install python3-specfile go2rpm go-vendor-tools
+dnf -y install go2rpm go-vendor-tools
 spectool -g sing-box.spec
 go_vendor_archive create sing-box.spec
 fedpkg srpm
