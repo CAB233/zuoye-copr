7713ec413b924febb7bd52faf8e9fc2e4e062130
CAB233 <yidaduizuoye@outlook.com>
2026-05-16 17:41:19 +0800

    sing-box: adjust spec

--- copr/sing-box/.srpm.bash
+++ copr/sing-box/.srpm.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf -y install go2rpm go-vendor-tools python3-specfile
+dnf -y install python3-specfile go2rpm go-vendor-tools
 spectool -g sing-box.spec
 go_vendor_archive create sing-box.spec
 fedpkg srpm
