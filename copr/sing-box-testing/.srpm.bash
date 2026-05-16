6280b255cf4e1e143206253d939d65b441c06a2d
CAB233 <yidaduizuoye@outlook.com>
2026-05-16 22:02:00 +0800

    Revert "build: update build scripts"
    This reverts commit f14c707d31b3938162c3fcdcbf529c6e33709f99.
    
    go-vendor-tools 依赖 python3-specfile

--- copr/sing-box-testing/.srpm.bash
+++ copr/sing-box-testing/.srpm.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf -y install go2rpm go-vendor-tools
+dnf -y install python3-specfile go2rpm go-vendor-tools
 spectool -g sing-box-testing.spec
 go_vendor_archive create sing-box-testing.spec
 fedpkg srpm
