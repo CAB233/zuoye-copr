c297cd9db2f960bb5ca2a07723a6c2c1b370fdf1
CAB233 <yidaduizuoye@outlook.com>
2026-06-04 10:08:06 +0800

    nexttrace: fix build on copr
    copr always uses f43 to make srpm :(

--- copr/nexttrace/.srpm.bash
+++ copr/nexttrace/.srpm.bash
@@ -3,6 +3,8 @@ set -euo pipefail
 
 SPEC=nexttrace.spec
 
+# Need latest golang to vendor.
+dnf -y --repofrompath='golang,https://download.copr.fedorainfracloud.org/results/@go-sig/golang-rawhide/fedora-$releasever-$basearch/' install golang
 dnf -y install python3-specfile go2rpm go-vendor-tools
 spectool -g "$SPEC"
 
