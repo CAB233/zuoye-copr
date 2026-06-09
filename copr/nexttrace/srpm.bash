89354402005d2a8e3f02fde1670b2aaca7c0ac8c
CAB233 <yidaduizuoye@outlook.com>
2026-06-09 23:46:32 +0800

    nexttrace: adjust srpm.bash

--- copr/nexttrace/srpm.bash
+++ copr/nexttrace/srpm.bash
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-SPEC=nexttrace.spec
+SPEC="$(basename $(pwd)).spec"
 
 # Need latest golang to vendor.
 dnf -y --repofrompath='golang,https://download.copr.fedorainfracloud.org/results/@go-sig/golang-rawhide/fedora-$releasever-$basearch/' install golang
-dnf -y install python3-specfile go2rpm go-vendor-tools
 spectool -g "$SPEC"
-
 go_vendor_archive create "$SPEC"
-
 fedpkg srpm
